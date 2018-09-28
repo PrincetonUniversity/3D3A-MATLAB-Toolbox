0893c521a7b9598650f24b173ad043d0934b8990
Rahulram Sridhar <rahulram@princeton.edu>
2018-09-28 13:43:21 -0400

    Make minor updates to various functions
    Update conventions

--- dsp/phaseSpec.m
+++ dsp/phaseSpec.m
@@ -20,7 +20,7 @@ function outputSpec = phaseSpec(inputIR,type,tol,DIM)
 %
 %   See also UNWRAP, ANGLE, FFT.
 
-%   ==============================================================================
+%   =======================================================================
 %   This file is part of the 3D3A MATLAB Toolbox.
 %   
 %   Contributing author(s), listed alphabetically by last name:
@@ -30,34 +30,39 @@ function outputSpec = phaseSpec(inputIR,type,tol,DIM)
 %   
 %   MIT License
 %   
-%   Copyright (c) 2017 Princeton University
+%   Copyright (c) 2018 Princeton University
 %   
-%   Permission is hereby granted, free of charge, to any person obtaining a copy
-%   of this software and associated documentation files (the "Software"), to deal
-%   in the Software without restriction, including without limitation the rights
-%   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-%   copies of the Software, and to permit persons to whom the Software is
-%   furnished to do so, subject to the following conditions:
+%   Permission is hereby granted, free of charge, to any person obtaining a
+%   copy of this software and associated documentation files (the 
+%   "Software"), to deal in the Software without restriction, including 
+%   without limitation the rights to use, copy, modify, merge, publish, 
+%   distribute, sublicense, and/or sell copies of the Software, and to 
+%   permit persons to whom the Software is furnished to do so, subject to 
+%   the following conditions:
 %   
-%   The above copyright notice and this permission notice shall be included in all
-%   copies or substantial portions of the Software.
+%   The above copyright notice and this permission notice shall be included
+%   in all copies or substantial portions of the Software.
 %   
-%   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-%   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-%   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-%   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-%   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-%   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-%   SOFTWARE.
-%   ==============================================================================
+%   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+%   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+%   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+%   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+%   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+%   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+%   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%   =======================================================================
 
 narginchk(1,4);
 
-inputIRSize = size(inputIR);
+if ~ismatrix(inputIR)
+    error('inputIR can be a scalar, vector, or matrix, only.');
+end
+
 if nargin < 4
-    DIM = 1;
     if isvector(inputIR)
-        inputIR = shiftdim(inputIR);
+        [~,DIM] = max(size(inputIR));
+    else
+        DIM = 1;
     end
 end
 if nargin < 3
@@ -67,13 +72,13 @@ if nargin < 2
     type = 'pv';
 end
 
-if strcmpi(type,'unwrap')
-    outputSpec = unwrap(angle(fft(inputIR,[],DIM)),tol,DIM);
-else
-    outputSpec = angle(fft(inputIR,[],DIM));
+switch lower(type)
+    case 'pv'
+        outputSpec = angle(fft(inputIR,[],DIM));
+    case 'unwrap'
+        outputSpec = unwrap(angle(fft(inputIR,[],DIM)),tol,DIM);
+    otherwise
+        error('Invalid type specification');
 end
 
-outputSpec = reshape(outputSpec,inputIRSize);
-
 end
-
