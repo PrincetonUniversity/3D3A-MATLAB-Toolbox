f96a52d36de2b00653a9375418308bd890f8ddc9
Joe Tylka <josephgt@princeton.edu>
2018-10-23 15:42:36 -0400

    Add getTimeVec

---
+++ dsp/getTimeVec.m
@@ -0,0 +1,44 @@
+function t = getTimeVec(Fs,N)
+%GETTIMEVEC Vector of linearly-spaced time values.
+%   T = GETTIMEVEC(FS,N) returns a length N column-vector of linearly-
+%   spaced time values ranging from 0 to (N - 1)/FS. If FS is specified in
+%   Hz (samples per second), then T is returned in seconds; if specified in
+%   kHz, then T is returned in milliseconds.
+%
+%   See also GETFREQVEC.
+
+%   =======================================================================
+%   This file is part of the 3D3A MATLAB Toolbox.
+%   
+%   Contributing author(s), listed alphabetically by last name:
+%   Joseph G. Tylka <josephgt@princeton.edu>
+%   3D Audio and Applied Acoustics (3D3A) Laboratory
+%   Princeton University, Princeton, New Jersey 08544, USA
+%   
+%   MIT License
+%   
+%   Copyright (c) 2018 Princeton University
+%   
+%   Permission is hereby granted, free of charge, to any person obtaining a
+%   copy of this software and associated documentation files (the 
+%   "Software"), to deal in the Software without restriction, including 
+%   without limitation the rights to use, copy, modify, merge, publish, 
+%   distribute, sublicense, and/or sell copies of the Software, and to 
+%   permit persons to whom the Software is furnished to do so, subject to 
+%   the following conditions:
+%   
+%   The above copyright notice and this permission notice shall be included
+%   in all copies or substantial portions of the Software.
+%   
+%   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+%   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+%   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+%   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+%   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+%   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+%   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%   =======================================================================
+
+t = (0:N-1).'/Fs;
+
+end
