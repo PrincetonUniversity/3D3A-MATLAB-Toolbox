f14e43f03b61934fea6a885f3c373c49a67ecbf0
Joe Tylka <josephgt@princeton.edu>
2018-10-31 21:58:02 -0400

    Add audible energy and scharer2009 metric functions

---
+++ metrics/estimateAudibleEnergy.m
@@ -0,0 +1,64 @@
+function E = estimateAudibleEnergy(x, Fs, FRANGE)
+%ESTIMATEAUDIBLEENERGY Total energy in auditory critical bands.
+%   E = ESTIMATEAUDIBLEENERGY(X,FS) computes the total audible energy E for
+%   a signal X given at sample rate FS. The total audible energy is
+%   computed as the sum of the signal energies in critical bands, modeled
+%   here by ERB-spaced (equivalent rectangular bandwidth) gammatone filters.
+%
+%   E = ESTIMATEAUDIBLEENERGY(X,FS,[FL,FH]) computes the total audible
+%   energy in the frequency range [FL,FH]. The default is [20,20000].
+%
+%   See also GETGAMMATONEFILTERS.
+
+%   ==============================================================================
+%   This file is part of the 3D3A MATLAB Toolbox.
+%   
+%   Contributing author(s), listed alphabetically by last name:
+%   Joseph G. Tylka <josephgt@princeton.edu>
+%   3D Audio and Applied Acoustics (3D3A) Laboratory
+%   Princeton University, Princeton, New Jersey 08544, USA
+%   
+%   MIT License
+%   
+%   Copyright (c) 2018 Princeton University
+%   
+%   Permission is hereby granted, free of charge, to any person obtaining a copy
+%   of this software and associated documentation files (the "Software"), to deal
+%   in the Software without restriction, including without limitation the rights
+%   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%   copies of the Software, and to permit persons to whom the Software is
+%   furnished to do so, subject to the following conditions:
+%   
+%   The above copyright notice and this permission notice shall be included in all
+%   copies or substantial portions of the Software.
+%   
+%   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+%   SOFTWARE.
+%   ==============================================================================
+
+if nargin < 3
+    FRANGE = [20,20000];
+end
+
+if exist('erbspacebw','file') == 2
+    fc = erbspacebw(min(FRANGE),max(FRANGE));
+else
+    warning('erbspacebw from LTFAT not found, using 1/3-octave bands instead.');
+    numfc = round(log2(max(FRANGE)/min(FRANGE))/(1/3));
+    fc = logspace(log10(min(FRANGE)),log10(min(FRANGE)*2^(numfc/3)),numfc);
+end
+
+FFTLen = size(x,1);
+
+h = getGammatoneFilters(fc, Fs, FFTLen);
+Hmag = magSpec(h,1); % FFTLen-by-length(fc)
+
+X = fft(x,FFTLen,1);
+E = mean(((Hmag.')*(abs(X).^2)));
+
+end
---
+++ metrics/scharer2009.m
@@ -0,0 +1,68 @@
+function [E, fc] = scharer2009(h, h0, Fs, fc)
+%SCHARER2009 Scharer's spectral errors in auditory bands.
+%   C = SCHARER2009(X, [], FS, FC) computes errors in the energy spectrum
+%   of X (i.e., abs(fft(X)).^2) in auditory bands with center frequencies
+%   specified by FC. Auditory bands are modeled by gammatone filters, as
+%   implemented in the LTFAT toolbox.
+%   
+%   C = SCHARER2009(X, X0, FS, FC) computes errors relative to the energy
+%   spectrum of X0.
+%
+%   [C, FC] = SCHARER2009(...) returns the center frequencies also.
+%   
+%   Suggested center frequencies for the equivalent rectangular bandwidth
+%   (ERB) scale:
+%       fc = erbspacebw(f_low,f_high); % Needs LTFAT toolbox.
+
+%   ==============================================================================
+%   This file is part of the 3D3A MATLAB Toolbox.
+%   
+%   Contributing author(s), listed alphabetically by last name:
+%   Joseph G. Tylka <josephgt@princeton.edu>
+%   3D Audio and Applied Acoustics (3D3A) Laboratory
+%   Princeton University, Princeton, New Jersey 08544, USA
+%   
+%   MIT License
+%   
+%   Copyright (c) 2018 Princeton University
+%   
+%   Permission is hereby granted, free of charge, to any person obtaining a copy
+%   of this software and associated documentation files (the "Software"), to deal
+%   in the Software without restriction, including without limitation the rights
+%   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%   copies of the Software, and to permit persons to whom the Software is
+%   furnished to do so, subject to the following conditions:
+%   
+%   The above copyright notice and this permission notice shall be included in all
+%   copies or substantial portions of the Software.
+%   
+%   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+%   SOFTWARE.
+%   ==============================================================================
+
+%   References:
+%     [1] Scharer and Lindau (2009) Evaluation of Equalization Methods for
+%         Binaural Signals.
+
+FFTLen = size(h,1);
+H = abs(fft(h,FFTLen,1));
+if isempty(h0)
+    H0 = ones(size(h));
+else
+    if ~isequal(size(h),size(h0))
+        error('Reference and input signals must be the same length.');
+    end
+    H0 = abs(fft(h0,FFTLen,1));
+end
+
+g = getGammatoneFilters(fc, Fs, FFTLen);
+Gmag = abs(fft(g,FFTLen,1)); % FFTLen-by-length(fc)
+
+E = mag2db(sqrt(((Gmag.')*(abs(H).^2))./((Gmag.')*(abs(H0).^2)))); % Eq. (9)
+
+end
