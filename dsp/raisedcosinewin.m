3933779d64fef1358dd79a6602b5f15fd9cc5336
Rahulram Sridhar <rahulram@princeton.edu>
2018-12-11 18:18:44 -0500

    Add option to specify r > 0.5 for asymmetric window

--- dsp/raisedcosinewin.m
+++ dsp/raisedcosinewin.m
@@ -2,10 +2,11 @@ function w = raisedcosinewin(L,r)
 %RAISEDCOSINEWIN Raised-cosine window.
 %   w = RAISEDCOSINEWIN(L,r) returns an L-point raised-cosine window in the 
 %   column vector, w. r must be a vector with two elements, r1 and r2, both
-%   of which can take values in the range 0 to 0.5. The resulting window is
-%   a rectangular window where the first 100*r1 and last 100*r2 percent of 
-%   the samples of the window equal parts of a cosine. If r1 = r2, a Tukey
-%   window is returned. If r is not specified, r = [0.5,0.5] is assumed.
+%   of which can take values in the range 0 to 1 provided (r1+r2) <= 1. The 
+%   resulting window is a rectangular window where the first 100*r1 and 
+%   last 100*r2 percent of the samples of the window equal parts of a 
+%   cosine. If r1 = r2 = 0.5, a Tukey window is returned. If r is not 
+%   specified, r = [0.5,0.5] is assumed.
 %
 %   See also TUKEYWIN.
 
@@ -48,13 +49,19 @@ if nargin < 2
 end
 
 validateattributes(L,{'double'},{'scalar','nonempty','nonnan','finite',...
-    'integer','positive'},'raisedCosineWin','L',1)
+    'integer','positive'},'raisedcosinewin','L',1)
 validateattributes(r,{'double'},{'vector','nonempty','nonnan','finite',...
-    'real','nonnegative','<=',0.5,'numel',2},'raisedCosineWin','r',2)
+    'real','nonnegative','<=',1,'numel',2},'raisedcosinewin','r',2)
 
-wStart = tukeywin(L,2*r(1));
-wEnd = tukeywin(L,2*r(2));
-halfLen = floor((L+1)/2);
-w = [wStart(1:halfLen); wEnd(halfLen+1:end)];
+if (r(1)+r(2)) > 1
+    error('Invalid specification of values for r. sum(r) must be <= 1.')
+end
+
+winVec = tukeywin(2*L,r(1));
+wStart = winVec(1:L);
+winVec = tukeywin(2*L,r(2));
+wEnd = winVec(L+1:2*L);
+
+w = min([wStart,wEnd],[],2);
 
 end
