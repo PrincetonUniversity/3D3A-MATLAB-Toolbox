dd9226855733a5d624a299b15d3d84a8a58887c8
Rahulram Sridhar <rahulram@princeton.edu>
2019-03-14 09:17:51 -0400

    Add option to regularize using arbitrary profile

--- dsp/computeInverseFilter.m
+++ dsp/computeInverseFilter.m
@@ -1,10 +1,12 @@
 function hInv = computeInverseFilter(h,varargin)
 %COMPUTEINVERSEFILTER Compute an inverse filter.
 %   hInv = COMPUTEINVERSEFILTER(h) returns the inverse of h without any
-%   regularization. h may be a vector, or matrix with the data to be
-%   inverted stored as columns. If h is a vector, hInv is a column vector 
-%   with the same length as h. If h is a matrix, hInv has the same 
-%   dimensions as h. This command may also be specified as: 
+%   regularization. 
+%       If h is a vector of length N, hInv will be a length-N column 
+%       vector. 
+%       If h is a matrix, each column is inverted and hInv will have the 
+%       same dimensions as h. 
+%   This command may also be specified as: 
 %       hInv = COMPUTEINVERSEFILTER(h,'direct');
 %
 %   ___ = COMPUTEINVERSEFILTER(h,'piecewise') returns the inverse of h with
@@ -12,7 +14,9 @@ function hInv = computeInverseFilter(h,varargin)
 %   profile, with regularization performed outside the range [w1,w2], where 
 %   w1 = 0.1 and w2 = 0.9, are normalized frequencies (normalized with 
 %   respect to the Nyquist frequency). The amount of regularization, eps, 
-%   applied is 0.001.
+%   applied is 0.001. If h is a matrix, the same profile is used to
+%   regularize the inverse of each column in h. This regularization
+%   approach is described in Farina [3].
 %
 %   ___ = COMPUTEINVERSEFILTER(h,'piecewise',PARAMS) specifies parameters 
 %   for regularization. PARAMS specifies the regularization frequency range 
@@ -20,8 +24,11 @@ function hInv = computeInverseFilter(h,varargin)
 %   regularization function as a 3x1 row vector [w1,w2,eps]. Multiple 
 %   "pieces" are specified as separate rows with eps being the amount of 
 %   regularization applied in the range [w1,w2]. w1 and w2 are normalized 
-%   frequencies with 0 corresponding to DC and 1 the Nyquist frequency.
-%   EXAMPLES:
+%   frequencies with 0 corresponding to DC and 1 the Nyquist frequency. If 
+%   h is a matrix, the same profile is used to regularize the inverse of 
+%   each column in h. To apply a different regularization for each column,
+%   see the 'profile' input option specified next.
+%   EXAMPLES for specifying PARAMS:
 %       1. [0,1,0] - no regularization.
 %       2. [0,0.1,0.001; 0.2,0.8,0; 0.9,1,0.001] - "shelf" profile with 
 %       both low and high shelves. Here, regularization is performed below 
@@ -29,6 +36,40 @@ function hInv = computeInverseFilter(h,varargin)
 %       0.8. Transitions between 0.1 and 0.2, and 0.8 and 0.9 are linear. 
 %       No regularization is performed between 0.2 and 0.8 since eps is set 
 %       to 0 in this range.
+%   This regularization approach is described in Farina [3].
+%
+%   ___ = COMPUTEINVERSEFILTER(h,'profile',{PROFILE,B}) specifies a 
+%   PROFILE for regularization, along with a corresponding B which scales 
+%   PROFILE either uniformly or element-by-element. 
+%       If PROFILE is a vector, it must be an impulse response with length 
+%       equal to the length of the signals in h. PROFILE is then used to
+%       regularize each signal in h. B can be a scalar or vector in this 
+%       case. If B is a vector, it must have the same length as PROFILE.
+%       If PROFILE is a matrix, it must store impulse responses as columns
+%       and have the same dimensions as h. Each column in PROFILE is then
+%       used to regularize corresponding columns in h. B can be a scalar, 
+%       vector, or matrix in this case. 
+%           If B is a vector, it must have as many elements as the number
+%           of columns in PROFILE.
+%           If B is a matrix, it must have the same dimensions as PROFILE.
+%   The appropriate regularization profile is extracted from the frequency 
+%   response of PROFILE. If PROFILE is not specified, a 4th-order 
+%   Butterworth bandstop filter with normalized cutoff frequencies of 0.1 
+%   and 0.9 (normalized with respect to the Nyquist frequency) is used to
+%   regularize each signal in h. If B is not specified, B = 1 is assumed. 
+%   To only specify B and use the default PROFILE, specify PROFILE as []. A
+%   version of this regularization approach is decribed by Kirkeby and
+%   Nelson [2].
+%
+%   ___ = COMPUTEINVERSEFILTER(h,'profile',{PROFILE,B,TARGET}) optionally
+%   specifies a target response. The default TARGET is a zero delay unit
+%   impulse.
+%       If TARGET is a vector, the same TARGET is used to invert all
+%       signals in h. TARGET must be an impulse response with length equal
+%       to the length of the signals in h.
+%       If TARGET is a matrix, it must store impulse responses as columns
+%       and have the same dimensions as h. Each column in TARGET is then
+%       used when inverting corresponding columns in h.
 %
 %   ___ = COMPUTEINVERSEFILTER(h,'gardner1994') returns the inverse of h 
 %   with frequency-dependent regularization described by Gardner and Martin 
@@ -84,6 +125,10 @@ function hInv = computeInverseFilter(h,varargin)
 %   References:
 %       [1] Gardner and Martin (1994) - HRTF Measurements of a KEMAR Dummy 
 %       Head Microphone.
+%       [2] Kirkeby and Nelson (1999) - Digital Filter Design for Inversion 
+%       Problems in Sound Reproduction.
+%       [3] Farina (2007) - Advancements in impulse response measurements 
+%       by sine sweeps.
 
 narginchk(1,3);
 
@@ -107,6 +152,105 @@ switch lower(TYPE)
         pProfile = repmat([pProfileHalf;...
             flipud(pProfileHalf(2:(end-1),1))],1,numChs);
         hInv = ifft(conj(H)./((conj(H).*H)+pProfile),'symmetric');
+    case 'profile'
+        H = fft(h);
+        profile = PARAMS{1};     
+        if isempty(profile)
+            profile = extra{1};
+        end
+        switch length(PARAMS)
+            case 1
+                beta = extra{2};
+                T = repmat([1;zeros(hLen-1,1)],1,numChs);
+            case 2
+                beta = PARAMS{2};
+                if isempty(beta)
+                    beta = 1;
+                end
+                T = repmat([1;zeros(hLen-1,1)],1,numChs);
+            case 3
+                beta = PARAMS{2};
+                if isempty(beta)
+                    beta = 1;
+                end
+                T = PARAMS{3};
+            otherwise
+                error('Invalid options specification for ''profile''.')
+        end
+        validateattributes(profile,{'double'},{'2d','nonempty','nonnan',...
+            'finite'},'computeInverseFilter',['PROFILE for option:',...
+            ' ''profile'''])
+        
+        profile = shiftdim(profile);
+        if isvector(profile)
+            if length(profile) ~= hLen
+                error(['Signals in PROFILE must have the same length',...
+                    ' as signals in h'])
+            else
+                validateattributes(beta,{'double'},{'vector','nonempty',...
+                    'nonnan','finite'},'computeInverseFilter',...
+                    'B for option: ''profile''')
+                if isscalar(beta)
+                    beta = beta*ones(hLen,numChs);
+                else
+                    beta = shiftdim(beta);
+                    if size(beta) ~= size(profile)
+                        error(['If B is not a scalar, it must have the',...
+                            ' same dimensions as PROFILE'])
+                    else
+                        beta = repmat(beta,1,numChs);
+                    end
+                end
+                profile = fft(repmat(profile,1,numChs));
+            end
+        else
+            if size(profile) ~= size(h)
+                error(['When PROFILE is a matrix it must have the same',...
+                    ' dimensions as h'])
+            else
+                validateattributes(beta,{'double'},{'2d','nonempty',...
+                    'nonnan','finite'},'computeInverseFilter',...
+                    'B for option: ''profile''')
+                if isscalar(beta)
+                    beta = beta*ones(hLen,numChs);
+                elseif isvector(beta)
+                    beta = shiftdim(beta);
+                    if size(beta,1) ~= size(profile,2)
+                        error(['If B is not a vector and PROFILE is ',...
+                            'a matrix, the length of B must equal the ',...
+                            'number of columns in PROFILE.'])
+                    else
+                        beta = repmat(beta.',hLen,1);
+                    end
+                else
+                    if size(beta) ~= size(profile)
+                        error(['If B and PROFILE are matrices, they',...
+                            ' must have the same size'])
+                    end
+                end
+                profile = fft(profile);
+            end
+        end
+        
+        T = shiftdim(T);
+        if isvector(T)
+            if length(T) ~= hLen
+                error(['Signals in TARGET must have the same length',...
+                    ' as signals in h'])
+            else
+                target = fft(repmat(T,1,numChs));
+            end
+        else
+            if size(T) ~= size(h)
+                error(['When TARGET is a matrix it must have the same',...
+                    ' dimensions as h'])
+            else
+                target = fft(T);
+            end
+        end
+        
+        hInv = ifft((conj(H).*target)./((conj(H).*H)+...
+            (beta.*(conj(profile).*profile))),'symmetric');
     case 'gardner1994'
         % Extract avgRange from PARAMS
         indx = find(strcmpi(PARAMS,'avgRange'),1);
@@ -174,6 +318,18 @@ if ~isempty(opts)
                 {'double'},{'2d','nonempty','nonnan','finite','real',...
                 'ncols',3},'computeInverseFilter',['PARAMS for option:',...
                 ' ''piecewise''']));
+        case 'profile'
+            % Compute default profile
+            hLen = size(h,1);
+            [num,den] = butter(4,[0.1,0.9],'stop');
+            profile = filter(num,den,[1;zeros(hLen-1,1)]);
+            beta = 1;
+            addOptional(p,'PARAMS',{profile,beta},...
+                @(x)validateattributes(x,{'cell'},{'vector','nonempty'},...
+                'computeInverseFilter',['PARAMS ',...
+                'for option: ''profile''']));
+            extra = {profile,beta}; % Default values returned for 
+                                    % performing additional checks.
         case 'gardner1994'
             hLen = size(h,1);
             w1 = 2*(2/hLen); % 1 octave above smallest, non-zero freq.
