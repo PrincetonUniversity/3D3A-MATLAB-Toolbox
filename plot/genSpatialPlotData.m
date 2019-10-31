e7cf9ac40d4f3662af37fc4feff71f9d0eb44e32
Rahulram Sridhar <rahulram@princeton.edu>
2019-10-31 11:13:09 -0400

    Rename function

---
+++ plot/genSpatialPlotData.m
@@ -0,0 +1,102 @@
+function varargout = genSpatialPlotData(dataIn,posIn,varargin)
+%GENSPATIALPLOTDATA Format data for generating 2D or 3D spatial plots.
+%   Z = GENSPATIALPLOTDATA(D,S) takes a length-N vector, D, of data 
+%   corresponding to positions, S, specified as an N-by-2 matrix of 
+%   coordinate pairs, and returns a data matrix, Z, with dimensions P-by-Q,
+%   where P is the number of unique values in the second column of S and Q
+%   is the number of unique values in the first column of S.
+%   Nearest-neighbor interpolation and extrapolation of the data in D is
+%   performed if necessary. The position values in S are rounded to 5
+%   decimal places when extracting unique values to mitigate rounding
+%   errors.
+%
+%   Z = GENSPATIALPLOTDATA(D,S,'roundS',false) does not round values in S
+%   prior to extracting unique values.
+%
+%   [Z,X,Y] = GENSPATIALPLOTDATA(__) additionally returns the unique values 
+%   in the first column of S as the vector X, and the unique values in the
+%   second column of S as the vector Y.
+%
+%   Needs: MATLAB R2013a or later.
+%
+%   See also MESHGRID, SCATTEREDINTERPOLANT, UNIQUE.
+
+%   =======================================================================
+%   This file is part of the 3D3A MATLAB Toolbox.
+%   
+%   Contributing author(s), listed alphabetically by last name:
+%   Rahulram Sridhar <rahulram@princeton.edu>
+%   3D Audio and Applied Acoustics (3D3A) Laboratory
+%   Princeton University, Princeton, New Jersey 08544, USA
+%   
+%   MIT License
+%   
+%   Copyright (c) 2019 Princeton University
+%   
+%   Permission is hereby granted, free of charge, to any person obtaining a
+%   copy of this software and associated documentation files (the 
+%   "Software"), to deal in the Software without restriction, including 
+%   without limitation the rights to use, copy, modify, merge, publish, 
+%   distribute, sublicense, and/or sell copies of the Software, and to 
+%   permit persons to whom the Software is furnished to do so, subject to 
+%   the following conditions:
+%   
+%   The above copyright notice and this permission notice shall be included
+%   in all copies or substantial portions of the Software.
+%   
+%   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+%   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+%   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+%   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+%   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+%   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+%   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%   =======================================================================
+
+narginchk(2,4);
+
+% Check inputs
+validateattributes(dataIn,{'double'},{'vector','nonempty','nonnan',...
+    'finite','real'},'genSpatialPlotData','D',1);
+dataIn = shiftdim(dataIn); % Force dataIn to be a column vector
+dataInLen = length(dataIn);
+validateattributes(posIn,{'double'},{'2d','nonempty','nonnan','finite',...
+    'real','size',[dataInLen,2]},'genSpatialPlotData','S',2);
+indx = find(strcmpi(varargin,'roundS'),1);
+if isempty(indx)
+    roundFlag = true;
+else
+    if indx == 1 && ~varargin{indx+1}
+        roundFlag = false;
+    else
+        roundFlag = true;
+    end
+end
+
+% Generate interpolation function
+if roundFlag
+    posInX = round(posIn(:,1),5);
+    posInY = round(posIn(:,2),5);
+else
+    posInX = posIn(:,1);
+    posInY = posIn(:,2);
+end
+interpF = scatteredInterpolant(posInX,posInY,dataIn,'nearest','nearest');
+
+% Extract unique position values and generate output data matrix
+[xPos,~,~] = unique(posInX);
+[yPos,~,~] = unique(posInY);
+[xPosGrid,yPosGrid] = meshgrid(xPos,yPos);
+dataMat = interpF(xPosGrid(:),yPosGrid(:));
+dataMat = reshape(dataMat,size(xPosGrid));
+
+switch nargout
+    case 1
+        varargout = {dataMat};
+    case 3
+        varargout = {dataMat,xPos,yPos};
+    otherwise
+        error('genSpatialPlotData expects request for 1 or 3 outputs.')
+end
+
+end
