9152ff90b8a541c58f60f6ca06d1f7b31535902e
Rahulram Sridhar <rahulram@princeton.edu>
2020-07-16 21:33:35 -0400

    Add correction to 'EW' method

--- binaural/computeSphereITD.m
+++ binaural/computeSphereITD.m
@@ -14,15 +14,20 @@ function ITD = computeSphereITD(a,sPos,TYPE)
 %       1. {'W'} - Woodworth formula (default). Antipodal ears are assumed.
 %       2. {'LF'} - Low-frequency limit formula (see, for example, 
 %       Kuhn [1]). Antipodal ears are assumed.
-%       3. {'EW',[EL;ER],R} - Extended Woodworth formula. EL and ER
+%       3. {'EW',[EL;ER],R,MaxF} - Extended Woodworth formula. EL and ER
 %       correspond to the left and right ear positions, respectively,
 %       each specified as the coordinate pair (az,el) where az and el 
 %       correspond to the azimuth and elevation, respectively, of the ear 
 %       specified in degrees in SOFA spherical coordinates. R corresponds 
-%       to source distance specified in meters. If R is not specified, R = 
-%       inf is assumed. If EL and ER are not specified, EL = (100,-10) and 
-%       ER = (260,-10) are assumed. The value of R specified here 
-%       supersedes the source distance specified in S.
+%       to source distance specified in meters and MaxF corresponds to the
+%       highest frequency at which this formula is applied and is used to 
+%       apply a correction as described by Sridhar and Choueiri [2]. 
+%       
+%       If R is not specified, R = inf is assumed. If EL and ER are not 
+%       specified, EL = (100,-10) and ER = (260,-10) are assumed. If MaxF
+%       is not specified, MaxF = inf is assumed (i.e., no correction is 
+%       applied). The value of R specified here supersedes the source 
+%       distance specified in S.
 
 %   =======================================================================
 %   This file is part of the 3D3A MATLAB Toolbox.
@@ -59,6 +64,9 @@ function ITD = computeSphereITD(a,sPos,TYPE)
 % Refs:
 %   [1]. Kuhn (1977) - Model for the interaural time differences in the 
 %   azimuthal plane.
+%
+%   [2]. Sridhar and Choueiri (2020) - Validation of the Minimum-Phase 
+%   Representation of the Rigid-Sphere Head-Related Transfer Function.
 
 narginchk(2,3);
 
@@ -83,6 +91,17 @@ switch lower(TYPE{1})
         sDirs = sofaC2cipicI(sPos); % Convert to interaural coordinates
         theta = deg2rad(sDirs(:,1)).'; % Row vector of azimuths in radians
     case 'ew'
+        if length(TYPE) < 4
+            corrFlag = false;
+        else
+            maxF = TYPE{4};
+            if isinf(maxF)
+                corrFlag = false;
+            else
+                corrFlag = true;
+            end
+        end
+        
         if length(TYPE) < 3
             R = inf;
         else
@@ -121,36 +140,63 @@ switch lower(TYPE{1})
         rho = R/a;
         delL = zeros(numPos,1);
         delR = zeros(numPos,1);
-        for ii = 1:numPos
-            delL(ii) = getDel(thetaL(ii),rho);
-            delR(ii) = getDel(thetaR(ii),rho);
+        if corrFlag
+            for ii = 1:numPos
+                delL(ii) = getDel(a,c,thetaL(ii),rho,maxF);
+                delR(ii) = getDel(a,c,thetaR(ii),rho,maxF);
+            end
+        else
+            for ii = 1:numPos
+                delL(ii) = getDel(a,c,thetaL(ii),rho);
+                delR(ii) = getDel(a,c,thetaR(ii),rho);
+            end
         end
-        ITD = (a/c)*(delL-delR);
+        ITD = delL-delR;
     otherwise
         error('Invalid TYPE{1} specification.')
 end
 
 end
 
-function D = getDel(T,R)
+function D = getDel(a,c,T,R,F)
 %GETDEL Compute delay value
-%   D = GETDEL(T) computes the phase delay, D, for a given input incidence
-%   angle, T, and non-dimensional source distance, R.
+%   D = GETDEL(A,C,T,R) computes the phase delay, D, for a given input 
+%   incidence angle, T, non-dimensional source distance, R, sphere radius,
+%   A, and speed of sound, C.
+%
+%   D = GETDEL(A,C,T,R,F) optionally includes a correction based on input
+%   frequency F, in Hz.
+
+if nargin < 5
+    corrFlag = false;
+else
+    muStar = pi*F*a/c;
+    cStar = c/(1+(0.5094*(2*muStar^2)^(-1/3)));
+    corrFlag = true;
+end
 
 if R == inf
     if T <= 90
-        D = -cosd(T);
+        D = -cosd(T)*(a/c);
     else
-        D = deg2rad(T-90);
+        if corrFlag
+            D = deg2rad(T-90)*(a/cStar);
+        else
+            D = deg2rad(T-90)*(a/c);
+        end
     end
 else
     T0 = acosd(1/R);
     hatG = sqrt(R^2-1);
     G = sqrt(R^2-(2*R*cosd(T))+1);
     if T < T0
-        D = G;
+        D = G*(a/c);
     else
-        D = hatG + deg2rad(T-T0);
+        if corrFlag
+            D = hatG*(a/c) + deg2rad(T-T0)*(a/cStar);
+        else
+            D = (hatG + deg2rad(T-T0))*(a/c);
+        end
     end
 end
 
