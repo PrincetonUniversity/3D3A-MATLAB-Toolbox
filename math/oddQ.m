48a1b75f3e4a7b6697e6faf0011666c896815866
Rahulram Sridhar <rahulram@princeton.edu>
2022-06-20 09:35:47 -0400

    Add new functions

---
+++ math/oddQ.m
@@ -0,0 +1,47 @@
+function F = oddQ(X)
+%ODDQ Check if an integer is odd.
+%   F = ODDQ(X) checks if each element in an array X is odd. The
+%   elements in X must be integers. F will be an array with the same
+%   dimensions as X and will contain logical values.
+%
+%   See also EVENQ.
+
+%   =======================================================================
+%   This file is part of the 3D3A MATLAB Toolbox.
+%   
+%   Contributing author(s), listed alphabetically by last name:
+%   Rahulram Sridhar <rahulram@princeton.edu>
+%   3D Audio and Applied Acoustics (3D3A) Laboratory
+%   Princeton University, Princeton, New Jersey 08544, USA
+%   
+%   MIT License
+%   
+%   Copyright (c) 2021 Princeton University
+%   
+%   Permission is hereby granted, free of charge, to any person obtaining a
+%   copy of this software and associated documentation files (the 
+%   "Software"), to deal in the Software without restriction, including 
+%   without limitation the rights to use, copy, modify, merge, publish, 
+%   distribute, sublicense, and/or sell copies of the Software, and to 
+%   permit persons to whom the Software is furnished to do so, subject to 
+%   the following conditions:
+%   
+%   The above copyright notice and this permission notice shall be included
+%   in all copies or substantial portions of the Software.
+%   
+%   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+%   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+%   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+%   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+%   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+%   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+%   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%   =======================================================================
+
+narginchk(1,1)
+
+validateattributes(X,{'numeric'},{'integer'},'oddQ','X',1)
+
+F = bitget(abs(X),1);
+
+end
